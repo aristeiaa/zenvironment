e8aa3c130618a52d1fb88f0c35ea2c2bbc3ae625
aristeiaa <aristeiaa@gmail.com>
2014-10-15 10:38:03 +0000

    updated to apt-get

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,18 +3,18 @@
 # 
 echo Installing Vim Master Race
 # install vim
-yum install vim -y -q
+apt-get install vim -y -q
 
 echo Installing Zsh
 # install zsh and oh my zsh
-yum install zsh -y -q
+apt-get install zsh -y -q
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 # set zsh as default shell
 chsh -s /bin/zsh
 
 echo Setting up Git
 # install git
-yum install git -y -q
+apt-get install git -y -q
 
 # setup git user
 git config --global user.name "aristeiaa"
@@ -23,4 +23,4 @@ git config --global core.editor vim
 git config --global merge.tool vimdiff
 
 echo Installing Tmux
-yum install tmux -y -q
+apt-get install tmux -y -q
